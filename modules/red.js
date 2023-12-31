f2cee47bfb458371b83ccac20ef4cf839093e3ad
TheCommCraft <79996518+TheCommCraft@users.noreply.github.com>
2023-12-31 16:23:39 +0100

    Add files via upload

---
+++ modules/red.js
@@ -0,0 +1,6 @@
+function main(runtime) {
+    // Scripts go in here
+    answerModPow();
+}
+
+const interval = setInterval(function () {try {window.runtime = document.getElementById('app')._reactRootContainer._internalRoot.current.child.stateNode.store.getState().scratchGui.vm.runtime; window.sprites = runtime.targets; window.backdrop = sprites[0]; window.global_variables = backdrop.variables; clearInterval(interval); console.log('Running main'); (main)(runtime);} catch (err) {}}, 1000);
